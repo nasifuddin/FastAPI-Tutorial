0c076bd169a65417b506680aa4e9d615aa77a356
Nasif Uddin <125158930+nasifuddin@users.noreply.github.com>
2024-01-21 16:46:50 +0600

    added new user creation - post method

--- myapi.py
+++ myapi.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI, Path
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 # creating a python dictionary which contains the information of the users
 users = {
-    47: {"name": "nasif", "age": 25, "email": "nasifuddinof@gmail.com"},
-    36: {"name": "fabi", "age": 26, "email": "fabinahian1@gmail.com"},
+    46: {"name": "nasif", "age": 25, "email": "nasifuddinof@gmail.com"},
+    37: {"name": "fabi", "age": 26, "email": "fabinahian1@gmail.com"},
 }
+
+
+class create_user(BaseModel):  # creating class for adding new user to the database
+    name: str
+    age: int
+    email: str
+
 
 # creating an end point
 """There are four endpoint methods used in an API:
@@ -23,7 +31,7 @@ def index():
     return {"This is the homepage"}
 
 
-# adding path parameters
+# adding path parameters (search by id)
 @app.get("/users/{user_id}")
 def get_users(
     user_id: int = Path(description="Provide the ID of the user", gt=0, lt=100)
@@ -59,3 +67,13 @@ def get_users_by_name(
     name: Optional[str] = None, test: int)
 
 """
+
+
+# creating new user by post method
+@app.post("/create-user/{id}")
+def create_new_user(id: int, user: create_user):
+    if id in users:
+        return {"error": "user_id already exists"}
+
+    users[id] = user
+    return users[id]
